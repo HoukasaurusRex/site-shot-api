aee809156a8675deb4d08d17498ff1418a57bee3
Pterobyte <jt1992@gmail.com>
2019-08-25 11:27:00 +0800

    add: open cors config

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
+const cors = require('./cors')
+
 const app = express()
 const errorHandler = (err, req, res, next) => {
   console.error(err)
@@ -15,6 +17,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('index'))
