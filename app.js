7055528890b8b04c4d4a982cb0ed0bacf8bf5c08
Pterobyte <jt1992@gmail.com>
2019-08-31 17:37:11 +0800

    add: cors module

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-
-const cors = require('./middleware/cors')
+const cors = require('cors')
 
 const app = express()
 const errorHandler = (err, req, res, next) => {
